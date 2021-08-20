145e0aa969fb525962f2f9cb5363eb40d6b8d324
Nelson Maldonado <play2nino@gmail.com>
2021-08-20 01:42:04 -0700

    3 components added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React from "react"
+import { BrowserRouter as Router, Route, Link } from "react-router-dom"
+import "./App.css"
+import Form from "./components/Form"
+import Home from "./components/Home"
+import Confirmation from "./components/Confirmation"
 
 const App = () => {
   return (
     <>
       <h1>Lambda Eats</h1>
+      <Router>
+        <Link to="/">Home</Link>
+        <Link to="/form">Form</Link>
+        <Link to="/about">About</Link>
+
+        <Route path="/about" component={About} />
+      </Router>
+
       <p>You can remove this code and create your own header</p>
     </>
-  );
-};
-export default App;
+  )
+}
+export default App
---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+function About() {
+  return (
+    <div>
+      <h1>Welcom Home</h1>
+      <h3>
+        This Pizza app was founded 1 hour ago! Our goal is to deliver you the
+        best pizza in San Francisco
+      </h3>
+    </div>
+  )
+}
+
+export default About
